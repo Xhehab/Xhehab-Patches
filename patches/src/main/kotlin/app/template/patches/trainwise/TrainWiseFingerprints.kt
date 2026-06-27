a474afa032079464bec1e0b805bb930cba4a6010
Shehab <being.shehab@gmail.com>
2026-06-27 14:48:47 +0600

    chore: skip TrainWise plan page flash

--- patches/src/main/kotlin/app/template/patches/trainwise/TrainWiseFingerprints.kt
+++ patches/src/main/kotlin/app/template/patches/trainwise/TrainWiseFingerprints.kt
@@ -11,3 +11,22 @@ val CustomerInfoMapFingerprint = Fingerprint(
     returnType = "Ljava/util/Map;",
     accessFlags = listOf(AccessFlags.PRIVATE, AccessFlags.STATIC, AccessFlags.FINAL)
 )
+
+val CommonGetCustomerInfoFingerprint = Fingerprint(
+    definingClass = "Lcom/revenuecat/purchases/hybridcommon/CommonKt;",
+    name = "getCustomerInfo",
+    parameters = listOf("Lcom/revenuecat/purchases/hybridcommon/OnResult;"),
+    returnType = "V",
+    accessFlags = listOf(AccessFlags.PUBLIC, AccessFlags.STATIC, AccessFlags.FINAL)
+)
+
+val CommonLogInFingerprint = Fingerprint(
+    definingClass = "Lcom/revenuecat/purchases/hybridcommon/CommonKt;",
+    name = "logIn",
+    parameters = listOf(
+        "Ljava/lang/String;",
+        "Lcom/revenuecat/purchases/hybridcommon/OnResult;"
+    ),
+    returnType = "V",
+    accessFlags = listOf(AccessFlags.PUBLIC, AccessFlags.STATIC, AccessFlags.FINAL)
+)
